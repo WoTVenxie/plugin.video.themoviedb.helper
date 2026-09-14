f006706549390783bf35a480918079bd730d0192
WoTVenxie <109520915+WoTVenxie@users.noreply.github.com>
2026-09-14 14:43:39 +0100

    Add sync classes for FlickList data types

---
+++ resources/tmdbhelper/lib/sync/flicklist/synctype.py
@@ -0,0 +1,65 @@
+from tmdbhelper.lib.addon.consts import HALFDAY_EXPIRY
+from tmdbhelper.lib.sync.flicklist.datatype import (
+    FlickListDataType,
+    FlickListDataTypeEpisodesInShows,
+    FlickListDataTypeShowsToEpisodes,
+)
+from tmdbhelper.lib.sync.datatype import timerlock
+
+
+class SyncWatched(FlickListDataTypeEpisodesInShows):
+    keys = (
+        'plays',
+        'last_watched_at',
+        'reset_at',
+    )
+    last_activities_key = 'watched_at'
+    method = 'watched'
+
+
+class SyncPlayback(FlickListDataTypeShowsToEpisodes):
+    keys = (
+        'progress',
+        'paused',
+        'updated_at',
+    )
+    last_activities_key = 'updated_at'
+    method = 'playback'
+    key_prefix = 'playback'
+
+
+class SyncRatings(FlickListDataType):
+    keys = (
+        'rating',
+        'rated_at',
+    )
+    last_activities_key = 'rated_at'
+    method = 'ratings'
+
+
+class SyncWatchlist(FlickListDataType):
+    keys = (
+        'status',
+        'progress',
+        'added_at',
+        'started_at',
+        'completed_at',
+    )
+    last_activities_key = 'added_at'
+    method = 'watchlist'
+    key_prefix = 'watchlist'
+
+
+class SyncUpNext(FlickListDataTypeShowsToEpisodes):
+    keys = (
+        'next_episode_id',
+        'next_episode_aired_at',
+    )
+    last_activities_key = 'watched_at'
+    method = 'up_next'
+    expiry_time = HALFDAY_EXPIRY
+
+    @timerlock
+    def sync_func(self):
+        """Get next episodes from FlickList."""
+        return self.get_response_sync()
